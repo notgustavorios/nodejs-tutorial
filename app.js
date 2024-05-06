20e7f8822f9646f8c8ead697f15da4b102cba710
gustavo pc <gustavorioslife@gmail.com>
2024-05-06 15:29:43 -0700

    2 hours in. Installed two bootstrap and lodash as examples

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const http = require('http')
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.end('Welcome to our homepage')
-    }
-    if(req.url === '/about'){
-        res.end('Welcome to our history')
-    }
-    else{
-        res.end(`
-        <h1>OOPS!</h1>
-        <p> We can't seem to find the page you're looking for</p>
-        <a href="/">Back home</a>
-        `)
-    }
-    
-})
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-server.listen(5000)
+// global dependency - use it in any package
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+
+const _ = require('lodash')
+
+const items = [1,[2],[3,[4]]]
+console.log(items);
+const newItems = _.flattenDeep(items)
+console.log(newItems)
